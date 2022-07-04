0ed31a103f1d09312ba647ee06b9f8eec7d255b1
EugeneSDET <108670347+EugeneSDET@users.noreply.github.com>
2022-07-04 21:00:27 +0300

    Addition_2
    Addition of new scripts.

---
+++ module_1_task_6_11_reg1.py
@@ -0,0 +1,47 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+try: 
+    link = "http://suninjuly.github.io/registration1.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    input1 = browser.find_element(By.CSS_SELECTOR, ".first_block .first")
+    input1.send_keys("Eugene")
+    input2 = browser.find_element(By.CSS_SELECTOR, ".first_block .second")
+    input2.send_keys("Bliznakov")
+    input3 = browser.find_element(By.CSS_SELECTOR, ".first_block .third")
+    input3.send_keys("Smolensk67@mail.ru")
+    # input4 = browser.find_element(By.XPATH, '//input[@placeholder="Input your phone:"]')
+    # input4.send_keys("+79111117777")
+    # input5 = browser.find_element(By.XPATH, '//input[@placeholder="Input your address:"]')
+    # input5.send_keys("Russia")
+    # button = browser.find_element(By.XPATH, '//button[@type="submit"]')
+    # button.click()
+    # Проверяем заполнение форм
+    time.sleep(1)
+
+    # Отправляем заполненную форму
+    button = browser.find_element(By.CSS_SELECTOR, "button.btn")
+    button.click()
+
+    # Проверяем, что смогли зарегистрироваться
+    # ждем загрузки страницы
+    time.sleep(5)
+
+    # находим элемент, содержащий текст
+    welcome_text_elt = browser.find_element(By.TAG_NAME, "h1")
+    # записываем в переменную welcome_text текст из элемента welcome_text_elt
+    welcome_text = welcome_text_elt.text
+
+    # с помощью assert проверяем, что ожидаемый текст совпадает с текстом на странице сайта
+    assert "Congratulations! You have successfully registered!" == welcome_text
+
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
+
+# не забываем оставить пустую строку в конце файла
