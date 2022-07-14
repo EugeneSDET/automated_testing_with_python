e34453db38365464cf864792b188622daef2a5fd
EugeneSDET <108670347+EugeneSDET@users.noreply.github.com>
2022-07-14 22:35:45 +0300

    Changing the name of function
    A small mending.

--- project_1/test_items.py
+++ project_1/test_items.py
@@ -6,7 +6,7 @@ from selenium.webdriver.common.by import By
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
-def test_guest_can_add_stuff_to_basket(browser):
+def test_check_basket_button_exists(browser):
     browser.get(link)
     time.sleep(10)
     assert browser.find_elements(By.CSS_SELECTOR, ".btn-add-to-basket"), 'basket button not found'
