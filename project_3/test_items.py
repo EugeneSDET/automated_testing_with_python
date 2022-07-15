8c4b71c53329a0f72cc4a18db2be9679fa88e4f7
EugeneSDET <108670347+EugeneSDET@users.noreply.github.com>
2022-07-15 22:24:52 +0300

    The study is done
    Latest educational projects.

---
+++ project_3/test_items.py
@@ -0,0 +1,18 @@
+import time
+
+from selenium.webdriver.common.by import By
+
+
+def button_is_present(browser):
+    try:
+        browser.implicitly_wait(10)
+        browser.find_elements(By.CLASS_NAME, "btn-add-to-basket")
+        time.sleep(5)
+        return True
+    except:
+        return False
+
+
+def test_cart_button_exist(browser):
+    assert button_is_present(browser) == True, "Button is not found"
+
